fb6fb56bf12ce52af3e19c19f72b829043e2b91d
Krystian Postek <krystian@postek.eu>
2019-10-14 22:31:48 +0200

    Added few tools
    decorator and bundled % regex

--- librus_tricks/tools.py
+++ librus_tricks/tools.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import re
 
 
 def get_next_monday(now=datetime.now()):
@@ -15,3 +16,34 @@ def get_actual_monday(now=datetime.now()):
             return now.date()
         now = now - timedelta(days=1)
     return
+
+
+def extract_percentage(grade):
+    """
+
+    :param librus_tricks.classes.SynergiaGrade grade:
+    :return:
+    """
+    for comment in grade.comments:
+        matches = re.findall('(\d+)%', comment.text)
+        if matches.__len__() > 0:
+            return float(matches[0])
+    return
+
+
+def return_extract_percentages(grades):
+    grades = [grade for grade in grades if extract_percentage(grade) is not None]
+    subjects = set([grade.subject.name for grade in grades])
+    categorized = {}
+    for subject in subjects:
+        categorized[subject] = []
+    for grade in grades:
+        categorized[grade.subject.name].append((grade, extract_percentage(grade)))
+    return categorized
+
+
+def no_cache(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **{**kwargs, 'expire': 0})
+
+    return wrapper
