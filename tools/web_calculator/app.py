3c9f499001bc132c68ed6f99b382245f093200c1
Krystian Postek <krystian@postek.eu>
2019-10-15 22:15:35 +0200

    Moved files to tools directory and added bootstrap

--- tools/web_calculator/app.py
+++ tools/web_calculator/app.py
@@ -1,9 +1,13 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(module)s:%(lineno)d - %(funcName)s - %(message)s')
+
 from flask import Flask, render_template, request
 from librus_tricks import minified_login, create_session, exceptions, cache
 from librus_tricks.tools import subjects_averages, percentage_average
 
 app = Flask(__name__)
-
+my_cache = cache.AlchemyCache()
 
 @app.route('/')
 def home_view():
@@ -12,11 +16,15 @@ def home_view():
 
 @app.route('/calc', methods=['POST'])
 def count_averages():
+    logging.info('Recived json %s', request.json)
     try:
-        session = create_session(request.json['email'], request.json['password'], cache=cache.DumbCache())
+        session = create_session(request.json['email'], request.json['password'], cache=my_cache)
+        logging.info('Session created %s', session)
     except exceptions.LibrusPortalInvalidPasswordError:
+        logging.info('Bad password')
         return render_template('bad_password.html')
     except exceptions.CaptchaRequired:
+        logging.info('Captcha')
         return render_template('damn_captcha.html')
     return render_template('table.html',
                            averages=subjects_averages(session.grades_categorized),
