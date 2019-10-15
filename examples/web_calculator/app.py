301b9904801db9a92936a73ed60f71d67ec364bc
Krystian Postek <krystian@postek.eu>
2019-10-15 18:53:08 +0200

    Created nice example

---
+++ examples/web_calculator/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from librus_tricks import minified_login, create_session, exceptions, cache
+from librus_tricks.tools import subjects_averages, percentage_average
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_view():
+    return render_template('home.html')
+
+
+@app.route('/calc', methods=['POST'])
+def count_averages():
+    try:
+        session = create_session(request.json['email'], request.json['password'], cache=cache.DumbCache())
+    except exceptions.LibrusPortalInvalidPasswordError:
+        return render_template('bad_password.html')
+    except exceptions.CaptchaRequired:
+        return render_template('damn_captcha.html')
+    return render_template('table.html',
+                           averages=subjects_averages(session.grades_categorized),
+                           percentages=percentage_average(session.grades()))
+
+
+@app.route('/dev')
+def dev_count_averages():
+    session = minified_login('krystian@postek.eu', 'cgAarXFV3xTmXQWvT7a78DEm')
+    return render_template('table.html',
+                           averages=subjects_averages(session.grades_categorized),
+                           percentages=percentage_average(session.grades()))
+
+
+if __name__ == '__main__':
+    app.run()
